7dc2a2357ceedb3beaeb7f15943785eb9b2674b7
ragingwind <ragingwind@gmail.com>
2017-03-30 13:41:36 +0900

    pwa: Add service-worker

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    "manifest.json",
+    "favicon.ico",
+    "bower_components/webcomponentsjs/webcomponents-lite.js",
+    'images/*'
+  ],
+  navigateFallback: 'index.html',
+  runtimeCaching: []
+};
